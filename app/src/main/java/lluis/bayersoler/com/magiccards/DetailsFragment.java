09fa7078db794615d9f633f46c5c351ef836ab50
Bayer <cava.staff@gmail.com>
2016-11-12 01:06:23 +0100

    Adaptem els layouts, el adaptador, controlador (activity) i la classe per donar total compatibilitat amb el DataBinding

--- app/src/main/java/lluis/bayersoler/com/magiccards/DetailsFragment.java
+++ app/src/main/java/lluis/bayersoler/com/magiccards/DetailsFragment.java
@@ -2,26 +2,28 @@ package lluis.bayersoler.com.magiccards;
 
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 import app.models.Card;
+import lluis.bayersoler.com.magiccards.databinding.FragmentDetailsBinding;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailsFragment extends Fragment {
-    private View fragment;
+    private FragmentDetailsBinding binding;
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -30,37 +32,38 @@ public class DetailsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+        //FragmentDetailsBinding binding = DataBindingUtil.setContentView(getActivity(), R.layout.fragment_details);
+        //setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        fragment = inflater.inflate(R.layout.fragment_details, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_details, container, false);
+        View view = binding.getRoot();
         Intent i = getActivity().getIntent();
         if(i != null){
             Card card = (Card) i.getSerializableExtra("card");
-            Log.d("CARDLOAD-> ",card.getName());
             if(card != null){
                 loadCardView(card);
+                getActivity().setTitle(card.getName());
+                ((AppCompatActivity) getActivity()).getSupportActionBar().setSubtitle(card.getType());
+
             }
         }
-        return fragment;
+        return view;
     }
 
     private void loadCardView(Card card){
-        TextView cardName = (TextView) fragment.findViewById(R.id.cardName);
-        cardName.setText(card.getName());
-        TextView cardType = (TextView) fragment.findViewById(R.id.cardType);
-        cardType.setText(card.getType());
-        TextView cardDescrption = (TextView) fragment.findViewById(R.id.cardDescrption);
-        cardDescrption.setText(card.getFlavor());
-
-        ImageView cardImage = (ImageView) fragment.findViewById(R.id.cardImage);
+        binding.setCard(card);
+        /*
+        ImageView cardImage = (ImageView) binding.getRoot().findViewById(R.id.cardImage);
         Glide.with(getContext())
                 .load(card.getImageUrl())
                 .dontAnimate()
                 .placeholder(R.drawable.card)
                 .into(cardImage);
+        */
     }
 
 }
